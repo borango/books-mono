81ac36dea6601f643dbd7c379be3c5ecd871b19f
Boran <boran@goegetap.name>
2025-08-04 18:01:46 +0000

    regression-testing full-preview build

--- commit_push_and_build_preview.sh
+++ commit_push_and_build_preview.sh
@@ -3,5 +3,11 @@ git commit -am "$*"
 git push
 
 # webhook on GitHub will trigger the Leanpub "preview" build
+# ... NOT 
+# (currently disabled so that author has control over build details, such as subset or full, or publish)
 
-./status_and_download.sh
+./trigger_preview.sh
+
+# triggering preview script (before) also downloads
+# ./status_and_download.sh
+
---
+++ manuscript/unit_test_chapter.md
@@ -0,0 +1,3 @@
+# Minimal Unit Test Chapter
+
+This manuscript file only consists of a title and a single paragraph.
