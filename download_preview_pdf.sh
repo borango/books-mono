7e264d90f2f8d2eb3c054134d24990e64693161f
Boran <boran@goegetap.name>
2025-08-05 12:54:19 +0000

    extending scripts to reflext preview subcase in download filename

--- download_preview_pdf.sh
+++ download_preview_pdf.sh
@@ -1,7 +1,7 @@
 curl \
   -L https://leanpub.com/s/AB35A37F7B714E129E2B2140F1C9E21C.pdf \
-  > dist/preview.pdf
+  > dist/preview_$1.pdf
 
-# -z "$(LC_TIME=C; stat -c %y dist/preview.pdf | date -f - -u +'%a, %d %b %Y %T GMT')" \
+# -z "$(LC_TIME=C; stat -c %y dist/preview_$1.pdf | date -f - -u +'%a, %d %b %Y %T GMT')" \
 
-xdg-open dist/preview.pdf
+xdg-open dist/preview_$1.pdf
--- trigger_preview.sh
+++ trigger_preview.sh
@@ -11,4 +11,4 @@ echo "Triggering a $scope_text Preview"
 
 curl -s -d "api_key=225BD9FEE6634FFDB6875DC5CA7B804C" https://leanpub.com/eco-travel/preview$scope_part.json > /dev/null
 
-./status_and_download.sh
+./status_and_download.sh $scope_text
