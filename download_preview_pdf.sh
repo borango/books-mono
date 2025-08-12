2b71abb292009382680bd1e86d8863e0bf231991
Boran <boran@goegetap.name>
2025-08-12 00:26:13 +0000

    differentiating the preview ID per book

--- download_preview_pdf.sh
+++ download_preview_pdf.sh
@@ -1,7 +1,7 @@
 DL_NAME="$(./get_slug_from_branch.sh)_preview_$1.pdf"
 
 curl \
-  -L https://leanpub.com/s/AB35A37F7B714E129E2B2140F1C9E21C.pdf \
+  -L https://leanpub.com/s/$(< preview_id.txt).pdf \
   > dist/$DL_NAME
 
 xdg-open dist/$DL_NAME
