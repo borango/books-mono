07be40052c3ac13555a598046fb08590399ebbe2
Boran <boran@goegetap.name>
2025-08-27 17:42:24 +0000

    fixing branch name retrieval for GH actions and merging with local build

--- pandoc.sh
+++ pandoc.sh
@@ -6,9 +6,13 @@ pandoc \
   --number-sections \
   --toc \
   --toc-depth=2 \
-  -o ../../mono/dist/$(../branch_to_filename.sh)_subset_pd.pdf \
+  -o ../$(../branch_to_filename.sh)_subset_pd.pdf \
   index.md \
   $(cat Subset.txt)
 
 
 cd ..
+
+if [ -d    ../mono/dist ]; then
+  mv *.pdf ../mono/dist/
+fi
